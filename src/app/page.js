d7340df0ed4d6575fbd11d81c82100a01f8789b0
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-06-16 09:48:29 -0500

    Add wakeupServer fetch with useEffect in Home page to initialize backend

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,37 @@
 "use client";
+import axios from "axios";
+import { useEffect } from "react";
 import Hero from "../components/Hero";
 import Header from "../components/Header";
+import Services from "../components/Services";
 import Footer from "../components/Footer";
 export default function Home() {
+  useEffect(() => {
+    async function wakeupServer() {
+      try {
+        const response = await fetch(
+          "https://bookingapp-backend-67cv.onrender.com"
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log("Services:", data);
+      } catch (error) {
+        console.error("Error fetching services:", error);
+      }
+    }
+
+    wakeupServer();
+  }, []);
   return (
     <>
       <Header />
       <main>
         <Hero />
+        <Services />
         <Footer />
       </main>
     </>
